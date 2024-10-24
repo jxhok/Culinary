158b038e8b407dcb44ee821e97910fdbbc278faf
Dman7713 <PeytonPrivratsky@gmail.com>
2024-10-24 09:33:39 -0700

    UI Update

--- Culinary Disaster 2/Assets/Scripts/GetInputFieldValue.cs
+++ Culinary Disaster 2/Assets/Scripts/GetInputFieldValue.cs
@@ -1,149 +1,104 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
+
 public class GetInputFieldValue : MonoBehaviour
 {
+    [System.Serializable]
+    public class Item
+    {
+        public TMP_InputField CostField;
+        public TMP_InputField AmountField;
+
+        public float GetCost()
+        {
+            float cost = -1;
+            float.TryParse(CostField.text, out cost);
+            return cost;
+        }
+
+        public float GetAmount()
+        {
+            float amount = -1;
+            float.TryParse(AmountField.text, out amount);
+            return amount;
+        }
+    }
+
     [SerializeField]
-    TMP_InputField Amt1;
+    private List<Item> items = new List<Item>();
+
     [SerializeField]
-    TMP_InputField Cost1;
+    private TMP_InputField markupValue;
     [SerializeField]
-    TMP_InputField Cost2;
+    private TMP_InputField Units;
     [SerializeField]
-    TMP_InputField Amt2;
-    [SerializeField]
-    TMP_InputField Cost3;
-    [SerializeField]
-    TMP_InputField Amt3;
-    [SerializeField]
-    TMP_InputField Cost4;
-    [SerializeField] 
-    TMP_InputField Amt4;
-    [SerializeField]
-    TMP_InputField Amt5;
-    [SerializeField] 
-    TMP_InputField Cost5;
-    [SerializeField]
-    TMP_InputField Cost6;
-    [SerializeField]
-    TMP_InputField Amt6;
-    [SerializeField]
-    TMP_InputField Cost7;
-    [SerializeField]
-    TMP_InputField Amt7;
-    [SerializeField]
-    TMP_InputField Cost8;
-    [SerializeField]
-    TMP_InputField Amt8;
-    [SerializeField]
-    TMP_InputField markupValue;
-    [SerializeField]
-    TMP_InputField Units;
-    [SerializeField]
-    TMP_Text caculatedNumber;
+    private TMP_Text calculatedNumber;
+
+    public void AddItem(TMP_InputField costField, TMP_InputField amountField)
+    {
+        Item newItem = new Item { CostField = costField, AmountField = amountField };
+        items.Add(newItem);
+    }
+
+    public void RemoveItem(int index)
+    {
+        if (index >= 0 && index < items.Count)
+        {
+            items.RemoveAt(index);
+        }
+    }
+
     public void Calculate()
     {
-        // finding the raw cost of item 1
-            float itemCost1 = -1;
-            Debug.Log(Amt1.text);
-            Debug.Log(float.TryParse(Cost1.text, out itemCost1));
-            float itemAmt1 = -1;
-            Debug.Log(float.TryParse(Amt1.text, out itemAmt1));
-            Debug.Log(itemAmt1 * itemCost1);
-        // cost item 2
-        float itemCost2 = -1;
-        Debug.Log(Amt2.text);
-        Debug.Log(float.TryParse(Cost2.text, out itemCost2));
-        float itemAmt2 = -1;
-        Debug.Log(float.TryParse(Amt2.text, out itemAmt2));
-        Debug.Log(itemAmt2 * itemCost2);
-        // item 3
-        float itemCost3 = -1;
-        Debug.Log(Amt3.text);
-        Debug.Log(float.TryParse(Cost3.text, out itemCost3));
-        float itemAmt3 = -1;
-        Debug.Log(float.TryParse(Amt3.text, out itemAmt3));
-        Debug.Log(itemAmt3 * itemCost3);
-        // item 4
-        float itemCost4 = -1;
-        Debug.Log(Amt4.text);
-        Debug.Log(float.TryParse(Cost1.text, out itemCost4));
-        float itemAmt4 = -1;
-        Debug.Log(float.TryParse(Amt4.text, out itemAmt4));
-        Debug.Log(itemAmt4 * itemCost4);
-        // item 5
-        float itemCost5 = -1;
-        Debug.Log(Amt5.text);
-        Debug.Log(float.TryParse(Cost5.text, out itemCost5));
-        float itemAmt5 = -1;
-        Debug.Log(float.TryParse(Amt5.text, out itemAmt5));
-        Debug.Log(itemAmt5 * itemCost5);
-        // item 6
-        float itemCost6 = -1;
-        Debug.Log(Amt6.text);
-        Debug.Log(float.TryParse(Cost6.text, out itemCost6));
-        float itemAmt6 = -1;
-        Debug.Log(float.TryParse(Amt6.text, out itemAmt6));
-        Debug.Log(itemAmt6 * itemCost6);
-        // item 7
-        float itemCost7 = -1;
-        Debug.Log(Amt7.text);
-        Debug.Log(float.TryParse(Cost7.text, out itemCost7));
-        float itemAmt7 = -1;
-        Debug.Log(float.TryParse(Amt7.text, out itemAmt7));
-        Debug.Log(itemAmt7 * itemCost7);
-        // item 8
-        float itemCost8 = -1;
-        Debug.Log(Amt8.text);
-        Debug.Log(float.TryParse(Cost8.text, out itemCost8));
-        float itemAmt8 = -1;
-        Debug.Log(float.TryParse(Amt8.text, out itemAmt8));
-        Debug.Log(itemAmt8 * itemCost8);
-        // find total raw cost
-        float totalCost = (itemAmt1 * itemCost1) + (itemAmt2 * itemCost2) + (itemAmt3 * itemCost3) + (itemAmt4 * itemCost4) + (itemAmt5 * itemCost5) + (itemAmt6 * itemCost6) + (itemAmt7 * itemCost7) + (itemAmt8 * itemCost8);
-        Debug.Log(totalCost);
-        //markup value
+        float totalCost = 0;
+
+        // Calculate total cost from all items
+        foreach (Item item in items)
+        {
+            float itemCost = item.GetCost();
+            float itemAmt = item.GetAmount();
+            totalCost += itemCost * itemAmt;
+        }
+
+        Debug.Log("Total Cost: " + totalCost);
+
+        // Get markup value
         float markup = -1;
-        Debug.Log(float.TryParse(markupValue.text, out markup));
-        //cost * markup
+        float.TryParse(markupValue.text, out markup);
+
+        // Calculate markup cost
         float markValue = markup * totalCost;
-        Debug.Log(markValue);
-        // amount of units
-        float unitsMade;
-        Debug.Log(float.TryParse(Units.text, out unitsMade));
-        // markup devided by number of units
+        Debug.Log("Marked-up Cost: " + markValue);
+
+        // Get units made
+        float unitsMade = -1;
+        float.TryParse(Units.text, out unitsMade);
+
+        // Calculate price per unit
         float ppu = markValue / unitsMade;
-        // make price per unit show on screen
-        caculatedNumber.text = ppu.ToString();
+        Debug.Log("Price Per Unit: " + ppu);
 
+        // Display price per unit
+        calculatedNumber.text = ppu.ToString();
+    }
 
-    }
-    public void Quit()
-    {
-       Application.Quit();
-    }
     public void Reset()
     {
-        Amt1.text = 0.ToString();
-        Amt2.text = 0.ToString();
-        Amt3.text = 0.ToString();
-        Amt4.text = 0.ToString();
-        Amt5.text = 0.ToString();
-        Amt6.text = 0.ToString();
-        Amt7.text = 0.ToString();
-        Amt8.text = 0.ToString();
-        Cost1.text = 0.ToString();
-        Cost2.text = 0.ToString();
-        Cost3.text = 0.ToString();
-        Cost4.text = 0.ToString();
-        Cost5.text = 0.ToString();
-        Cost6.text = 0.ToString();
-        Cost7.text = 0.ToString();
-        Cost8.text = 0.ToString();
-        markupValue.text = 0.ToString();
-        Units.text = 0.ToString();
-        caculatedNumber.text = 0.ToString();
-        
+        // Reset all input fields in items
+        foreach (Item item in items)
+        {
+            item.AmountField.text = "0";
+            item.CostField.text = "0";
+        }
 
+        markupValue.text = "0";
+        Units.text = "0";
+        calculatedNumber.text = "0";
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
